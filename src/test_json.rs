c74b5241f9127ef6a9153b9dbab5dd284684c424
Package <Aditrc@outlook.com>
2020-05-09 20:12:52 +0800

    save

--- src/test_json.rs
+++ src/test_json.rs
@@ -11,7 +11,27 @@ mod token {
     use regex::*;
     use std::str::*;
 
-    struct TokenData {}
+    enum Token {
+        Str(String),
+        Num(f64),
+        ArrS,
+        ArrE,
+        ObjS,
+        ObjE,
+        Comma,
+        Colon,
+    }
+
+    struct TokenData {
+        index: usize,
+        tokens: Vec<Token>,
+    }
+    impl TokenData {
+        #[inline]
+        pub fn save(&self) -> usize {
+            self.index
+        }
+    }
     type Flow = RecunsFlow<char, TokenData>;
 
     #[test]
@@ -23,7 +43,10 @@ mod token {
         let mut errors = vec![];
         let r = root.recuns();
         let r = do_iter(
-            TokenData {},
+            TokenData {
+                index: 0,
+                tokens: vec![],
+            },
             r,
             true,
             &mut errors,
@@ -35,19 +58,45 @@ mod token {
     }
 
     fn root(inp: char, data: &mut TokenData, eof: bool) -> Flow {
-        check_number(inp).unwrap_or(RecunsFlow::None)
+        let sp = data.save();
+        check_number(inp, sp).unwrap_or(RecunsFlow::None)
     }
 
     lazy_static! {
-        static ref num_start: Regex = Regex::new(r"[\-\d\.]").unwrap();
+        static ref number_regex: Regex =
+            Regex::new(r"-?(([1-9]\d*)|0)(\.\d+)?([eE][-+]?\d+)?").unwrap();
     }
-    fn check_number(inp: char) -> Option<Flow> {
-        if bop!(|| inp; ==; '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9') {
-            return |inp, data: &mut TokenData, eof| -> Flow { 
-                    RecunsFlow::End 
+    fn check_number(first: char, sp: usize) -> Option<Flow> {
+        #[inline(always)]
+        fn is_num_start(c: char) -> bool {
+            bop!(|| c; ==; '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+        }
+        #[inline(always)]
+        fn is_num(c: char) -> bool {
+            is_num_start(c) || bop!(|| c; ==; 'e', 'E', '+')
+        }
+        if is_num_start(first) {
+            let mut strs = vec![first];
+            return move |inp, data: &mut TokenData, eof| -> Flow {
+                if eof || inp == '\0' || !is_num(inp) {
+                    let s = strs.iter().collect::<String>();
+                    let f = s.parse::<f64>();
+                    match f {
+                        Ok(f) => {
+                            data.tokens.push(Token::Num(f));
+                        }
+                        Err(e) => {
+                            return RecunsFlow::Err(Arc::new(e));
+                        }
+                    }
+                } else {
+                    strs.push(inp);
+                    return RecunsFlow::None;
                 }
-                .rfcall()
-                .into();
+                RecunsFlow::End
+            }
+            .rfcall_next()
+            .into();
         };
         None
     }
